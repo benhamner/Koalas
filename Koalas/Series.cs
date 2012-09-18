de17abd24703bd828bd71c188acf0b8f9e0beb4a
Ben Hamner <ben@benhamner.com>
2012-09-18 14:58:50 -0700

    Added Series

---
+++ Koalas/Series.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Koalas
+{
+    public class Series<T> : IEnumerable<T> {
+        public List<T> Data;
+
+        public Series () {
+
+            Data = new List<T>();
+        }
+
+        public Series(int length)
+        {
+
+            Data = new List<T>(length);
+        } 
+
+        public IEnumerator<T> GetEnumerator() {
+            return Data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }
+}
